f8f9e71826af1026b5be0f74138e0dff102ba7ab
Asma Sadia <a.sadia51299@gmail.com>
2017-10-07 18:48:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,10 +8,9 @@ function removeFromPlaylist(playlist, artistName){
   return playlist
 }
 
-
 //Open objects.js
 //Assign an object to the variable playlist and initialize the object with a
-//key-value pair — the keys will be artist names and the values will be song titles.
+//key-value pair — the keys will be artist names and the values will be song titles. 
 //(What limitation does this impose on our playlist?)
 //Create a function updatePlaylist that accepts three parameters:
 //the playlist (an object), an artist name (a string), and a song title.
@@ -20,5 +19,5 @@ function removeFromPlaylist(playlist, artistName){
 //The function should return the whole playlist.
 //Create a function removeFromPlaylist that accepts two arguments
 //(the playlist object and the artist name).
-//The body of the function should delete the key-value pair from the playlist and
+//The body of the function should delete the key-value pair from the playlist and 
 //return the updated playlist.
